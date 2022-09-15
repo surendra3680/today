96dba3c65b839070193c5ee1df537e037672ceb1
surendra <rajputsurendra6004@gmail.com>
2022-09-15 18:41:39 +0530

    final

---
+++ script.js
@@ -0,0 +1,7 @@
+const html=document.documentElement;
+// html.children[0].children[4].textContent="no body";
+
+html.children[0].children[4].textContent="index"
+
+// html.children[1].style.backgroundColor="black"
+
